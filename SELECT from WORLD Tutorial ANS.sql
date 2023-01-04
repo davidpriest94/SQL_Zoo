76113f3e118cdf3bd73ba589deb3460903e1f3df
davidpriest94 <54874116+davidpriest94@users.noreply.github.com>
2023-01-04 14:11:09 -0800

    Create SELECT from WORLD Tutorial ANS.sql
    https://sqlzoo.net/wiki/SELECT_from_WORLD_Tutorial

---
+++ SELECT from WORLD Tutorial ANS.sql
@@ -0,0 +1,24 @@
+Q1. Read the notes about this table. Observe the result of running this SQL command to show the name, continent and population of all countries.
+
+SELECT name, continent, population 
+  FROM world;
+ 
+Q2. Show the name for the countries that have a population of at least 200 million. 200 million is 200000000, there are eight zeros.
+
+SELECT name 
+  FROM world
+  WHERE population >= 200000000; 
+  
+Q3. Give the name and the per capita GDP for those countries with a population of at least 200 million.
+
+SELECT name, GDP/population AS 'per capita GDP'
+  FROM world
+  WHERE population >= 200000000;
+  
+Q4. Show the name and population in millions for the countries of the continent 'South America'. Divide the population by 1000000 to get population in millions.
+
+SELECT name, population/1000000 AS 'population(millions)'
+  FROM world
+  WHERE continent = 'South America';
+  
+Q5. 
